63eec8b11c09065fc765bfad9eb7c6aecf130059
Piyush Panchal <piyushpanchal114@gmail.com>
2024-12-25 18:20:53 +0530

    go varable

---
+++ 01-variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	username := "piyush"
+	fmt.Println("username", username)
+	fmt.Printf("The variable type is %T \n", username)
+
+	isLoggedIn := true
+	fmt.Println("isLoggedIn", isLoggedIn)
+	fmt.Printf("The variable type is %T \n", isLoggedIn)
+
+}
