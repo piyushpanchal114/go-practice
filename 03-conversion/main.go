94c2c7d885b1293765a5ec3a9c2e71296a9c9f81
Piyush Panchal <piyushpanchal114@gmail.com>
2024-12-25 19:06:49 +0530

    conversion in golang

---
+++ 03-conversion/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to our pizza app")
+	fmt.Println("Please rate our pizza:")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating...", input)
+	fmt.Printf("Type of Rating is %T \n\n", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println("error", err)
+	} else {
+		fmt.Println("Added 1 to the user rating ....", numRating+1)
+	}
+
+}
