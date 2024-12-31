bee00a5f7a0daa8cce5c61aa2f0c550cd8ce9030
Piyush Panchal <piyushpanchal114@gmail.com>
2024-12-31 15:07:54 +0530

    buildapi: get all courses

--- 22-buildapi/main.go
+++ 22-buildapi/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -35,4 +37,10 @@ func main() {
 // serve home route
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to the First Ever Web Server</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
