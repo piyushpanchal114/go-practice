a3f78b920481dfce4ba2763cd74786fee506a02f
Piyush Panchal <piyushpanchal114@gmail.com>
2025-01-01 23:42:17 +0530

    update one course

--- 22-buildapi/main.go
+++ 22-buildapi/main.go
@@ -94,3 +94,23 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	return
 
 }
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update one Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from req
+	params := mux.Vars(r)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+}
