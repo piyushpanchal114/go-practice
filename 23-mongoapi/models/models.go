a609f8186a9ed08d39a0d4f3b3ef579ddf05b102
Piyush Panchal <piyushpanchal114@gmail.com>
2025-01-03 14:54:43 +0530

    mongoapi: models

--- 23-mongoapi/models/models.go
+++ 23-mongoapi/models/models.go
@@ -1,11 +1,7 @@
 package models
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Netflix struct {
-	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Movie   string             `json:"movie"`
-	Watched bool               `json:"watched"`
+	// ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Movie   string `json:"movie"`
+	Watched bool   `json:"watched"`
 }
