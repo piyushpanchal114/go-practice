6fe5783ce5d0afe1596b8cd615c2cfd56004e946
Piyush Panchal <piyushpanchal114@gmail.com>
2025-01-07 16:50:13 +0530

    mongoapi: seving apis

--- 23-mongoapi/main.go
+++ 23-mongoapi/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/piyushpanchal114/mongoapi/routers"
+)
 
 func main() {
 	fmt.Println("Mongo API")
+	r := routers.Router()
+	fmt.Println("Server is getting started...")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000...")
 }
